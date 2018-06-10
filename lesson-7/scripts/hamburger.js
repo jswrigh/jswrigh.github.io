b71defe683f1ee44866862e0f73c356e748cd28e
Sterling Wright <j.sterling.wright@gmail.com>
2018-06-09 21:41:21 -0400

    Start of lesson-7

---
+++ lesson-7/scripts/hamburger.js
@@ -0,0 +1,4 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function toggleHam() {
+	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
