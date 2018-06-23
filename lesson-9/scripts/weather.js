bc651cce3d77ad19b6dd81f8d4c3de1611b4226e
Sterling Wright <j.sterling.wright@gmail.com>
2018-06-23 17:49:20 -0400

    Starting lesson 9

---
+++ lesson-9/scripts/weather.js
@@ -0,0 +1,9 @@
+let weatherRequest = new XMLHttpRequest();
+let apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=4156210&units=imperial&APPID=123e236852641b9b3bfd755ffa553566";
+weatherRequest.open("GET",apiURL,true);
+weatherRequest.send();
+weatherRequest.onload =  function () {
+ let weatherData = JSON.parse(weatherRequest.responseText);
+ console.log(weatherData);
+ document.getElementById("current-temp").innerHTML = weatherData.list[0].main.temp;
+}
