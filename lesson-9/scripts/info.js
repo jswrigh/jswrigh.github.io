ca88a301dc80bd68ac4df65a55faeeb39d0dae4c
Sterling Wright <j.sterling.wright@gmail.com>
2018-06-23 20:40:02 -0400

    Ready to submit lesson 9
    Moved weather info to franklin-9. Made attribution data a link. Fixed navigation so links work.

--- lesson-9/scripts/info.js
+++ lesson-9/scripts/info.js
@@ -9,4 +9,11 @@ document.getElementById("franklin-motto").innerHTML = infoData.towns[0].motto;
 document.getElementById("franklin-founded").innerHTML = infoData.towns[0].yearFounded;
 document.getElementById("franklin-population").innerHTML = infoData.towns[0].currentPopulation;
 document.getElementById("franklin-rainfall").innerHTML = infoData.towns[0].averageRainfall;
+document.getElementById("greenville-motto").innerHTML = infoData.towns[1].motto;
+document.getElementById("greenville-founded").innerHTML = infoData.towns[1].yearFounded;
+document.getElementById("greenville-population").innerHTML = infoData.towns[1].currentPopulation;
+document.getElementById("greenville-rainfall").innerHTML = infoData.towns[1].averageRainfall;document.getElementById("springfield-motto").innerHTML = infoData.towns[3].motto;
+document.getElementById("springfield-founded").innerHTML = infoData.towns[3].yearFounded;
+document.getElementById("springfield-population").innerHTML = infoData.towns[3].currentPopulation;
+document.getElementById("springfield-rainfall").innerHTML = infoData.towns[3].averageRainfall;
 }
