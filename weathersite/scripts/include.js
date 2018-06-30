2f825173fa85a23fb84c26e70d74e2e0daca6709
Sterling Wright <j.sterling.wright@gmail.com>
2018-06-30 16:29:06 -0400

    Removed debug log from include.js

--- weathersite/scripts/include.js
+++ weathersite/scripts/include.js
@@ -1,13 +1,11 @@
 function include() {
-    console.log("IN FUNCTION");
   let file, xhttp; 
   let allElements = document.getElementsByTagName('*'); //get all elements on the page
 
   for (let i = 0; i < allElements.length; i++) { // process all the elements looking for the custom attribute 'includefile'
     file = allElements[i].getAttribute("includefile");  
     if (file) { // if the attribute is found, process an AJAX XMLHttpRequest
-        console.log("file: " + file);
-      xhttp = new XMLHttpRequest();
+       xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
         if (this.readyState == 4) {
           if (this.status == 200) { allElements[i].innerHTML = this.responseText;}
@@ -18,7 +16,6 @@ function include() {
 
        }
         let includeFile = 'includes/' + file;
-        console.log(includeFile)
         xhttp.open('GET', includeFile, true); // matching file names in includes folder
         xhttp.send();
         return;    
